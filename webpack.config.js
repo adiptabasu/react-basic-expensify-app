a51be0f35fddaf6ae70744497ac8d9c35537f090
adiptabasu <adiptabasu28@gmail.com>
2022-02-10 00:17:13 +0530

    Commited for dist folder setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,9 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = (env, argv) => {
-    // console.log('argv->', argv);
     const isProduction = argv.mode === 'production'
-    // const CSSExtract = new MiniCssExtractPlugin('styles.css');
     return {
         entry: './src/app.js',
-        // entry: './src/playground/hoc.js',
         plugins: [new MiniCssExtractPlugin()],
         output: {
             path: path.join(__dirname, 'public', 'dist'),
@@ -15,29 +12,12 @@ module.exports = (env, argv) => {
         },
         module: {
             rules: [{
-                loader: 'babel-loader',//this the babel parser
-                test: /\.js$/,//files to check for babel parsing
+                loader: 'babel-loader',
+                test: /\.js$/,
                 exclude: /node_modules/
             },
-            // {
-            //     test: /\.css$/,
-            //     exclude: /node_modules/,
-            //     use: [
-            //         'style-loader',
-            //         'css-loader'
-            //     ]
-            // },
             {
                 test: /\.s?css$/,
-                // exclude: /node_modules/,
-                // use: [
-                //     // Creates `style` nodes from JS strings
-                //     "style-loader",
-                //     // Translates CSS into CommonJS
-                //     "css-loader",
-                //     // Compiles Sass to CSS
-                //     "sass-loader",
-                // ]
                 use: [MiniCssExtractPlugin.loader,
                 {
                     loader: 'css-loader',
@@ -64,14 +44,12 @@ module.exports = (env, argv) => {
             client: {
                 progress: true,
             },
-            // contentBase: path.join(__dirname, 'public')
             static: {
-                directory: path.join(__dirname, 'public', 'dist'),
+                directory: path.join(__dirname, 'public'),
             },
             compress: true,
             port: 3000,
-            historyApiFallback: true//added for react-router
-            // publicPath: '/dist/'
+            historyApiFallback: true
         }
     }
 };
