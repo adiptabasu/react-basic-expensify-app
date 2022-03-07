8b71581ceee76fd8643cca16eb7ba8f00d405b3a
adiptabasu <adiptabasu28@gmail.com>
2022-03-08 00:40:54 +0530

    Committed for daily update

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,6 @@
 // import firebase from 'firebase';
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, set, update, remove, onValue, off } from 'firebase/database';
+import { getDatabase, ref, set, update, remove, onValue, off, push } from 'firebase/database';
 
 const firebaseConfig = {
     apiKey: "AIzaSyBUTHZ7Zc_lxGAzZWYCLlXVe72OYpnAYGo",
@@ -17,28 +17,28 @@ initializeApp(firebaseConfig);
 
 const db = getDatabase();
 
-set(ref(db), {
-    name: 'Adipta Basu',
-    age: 26,
-    isSingle: true,
-    stressLevel: 6,
-    job: {
-        title: 'SDE',
-        company: 'Google'
-    },
-    location: {
-        city: 'Kolkata',
-        country: 'India'
-    }
-})
-    .then(() => {
-        console.log('Data is saved');
-    })
-    .catch((error) => console.log('This failed', error));
+// set(ref(db), {
+//     name: 'Adipta Basu',
+//     age: 26,
+//     isSingle: true,
+//     stressLevel: 6,
+//     job: {
+//         title: 'SDE',
+//         company: 'Google'
+//     },
+//     location: {
+//         city: 'Kolkata',
+//         country: 'India'
+//     }
+// })
+//     .then(() => {
+//         console.log('Data is saved');
+//     })
+//     .catch((error) => console.log('This failed', error));
 
-set(ref(db, 'age'), 27);
+// set(ref(db, 'age'), 27);
 
-set(ref(db, 'location/city'), 'Ruby');
+// set(ref(db, 'location/city'), 'Ruby');
 
 // update(ref(db), {
 //     height: 160,
@@ -73,35 +73,85 @@ set(ref(db, 'location/city'), 'Ruby');
 //     })
 //     .catch((error) => console.log('This failed', error));
 
-onValue(ref(db), (dataSnapshot) => {
-    const val = dataSnapshot.val();
-    console.log('snap val is->', val);
-}, {
-    onlyOnce: false
-}
-);
+// onValue(ref(db), (dataSnapshot) => {
+//     const val = dataSnapshot.val();
+//     console.log('snap val is->', val);
+// }, {
+//     onlyOnce: false
+// }
+// );
 
-const cityLocValChange = onValue(ref(db, 'location/city'), (dataSnapshot) => {
-    const val = dataSnapshot.val();
-    console.log('snap val is->', val);
-}, {
-    onlyOnce: false
-}, (e) => {
-    console.log('Error while fetching data', e);
-}
-);
+// const cityLocValChange = onValue(ref(db, 'location/city'), (dataSnapshot) => {
+//     const val = dataSnapshot.val();
+//     console.log('snap val is->', val);
+// }, {
+//     onlyOnce: false
+// }, (e) => {
+//     console.log('Error while fetching data', e);
+// }
+// );
 
-setTimeout(() => {
-    set(ref(db, 'location/city'), 'Jadavpur');
-}, 5000);
+// setTimeout(() => {
+//     set(ref(db, 'location/city'), 'Jadavpur');
+// }, 5000);
 
-setTimeout(() => {
-    set(ref(db, 'location/city'), 'Jadavpur1');
-    //If there is only one subsciption and it needs to be unsubscribed,this works
-    // off(ref(db));
-    // For multiple-subscription and reverse, below works
-    cityLocValChange();
-}, 6000);
-setTimeout(() => {
-    set(ref(db, 'location/city'), 'Jadavpur2');
-}, 7000); 
+// setTimeout(() => {
+//     set(ref(db, 'location/city'), 'Jadavpur1');
+//     //If there is only one subsciption and it needs to be unsubscribed,this works
+//     // off(ref(db));
+//     // For multiple-subscription and reverse, below works
+//     cityLocValChange();
+// }, 6000);
+// setTimeout(() => {
+//     set(ref(db, 'location/city'), 'Jadavpur2');
+// }, 7000);
+
+// remove(ref(db));
+
+const expenses = [
+    {
+        id: 1,
+        description: 'First Description',
+        note: 'First note',
+        amount: 2500,
+        createdAt: 10
+    },
+    {
+        id: 2,
+        description: 'Second Description',
+        note: 'Second note',
+        amount: 200,
+        createdAt: 100
+    },
+    {
+        id: 3,
+        description: 'Third Description',
+        note: 'Third note',
+        amount: 5000,
+        createdAt: -10
+    }];
+remove(ref(db));
+expenses.forEach((expense) => push(ref(db, 'expenses'), expense));
+
+// set(ref(db, 'notes'), notes);
+// push(ref(db, 'notes'), {
+//     title: 'second title',
+//     body: 'This is second note'
+// });
+
+// update(ref(db, 'notes/-Mx_NHq8hQGq5qeJDjXl'), {
+//     body: 'Updated data'
+// });
+
+onValue(ref(db, 'expenses'), (snapshot) => {
+    const expensesStore = [];
+    snapshot.forEach((childSnap) => {
+        expensesStore.push({
+            id: childSnap.key,
+            ...childSnap.val()
+        })
+    })
+    console.log(expensesStore)
+}, { onlyOnce: false }, (e) => {
+    console.log('Error is ', e);
+});
